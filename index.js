89795c7a2311a01d0c5e4c49093bdf492841fb13
Shubham Tandale <shubhamtandale@outlook.in>
2022-04-07 23:11:56 +0530

    completed basic todo app

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Todo App')
+const formElement = document.getElementById('form');
+const inputElement = document.getElementById('todo');
+const listElement = document.getElementById('todo-list');
+
+const submitHandler = (event) => {
+  event.preventDefault()
+
+  listElement.innerHTML = listElement.innerHTML + '<li>' + inputElement.value + '</li>'
+}
+
+formElement.addEventListener('submit', submitHandler)
+
+window.onbeforeunload = () => {
+  formElement.removeEventListener('submit', submitHandler)
+}
